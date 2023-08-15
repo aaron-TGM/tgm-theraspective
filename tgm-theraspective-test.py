156f623a8e67d11c0d09ff9355a03e7115ebf976
aaron-TGM <aaron@tuffghostmedia.com>
2023-08-14 21:24:08 -0700

    adding selenium

--- tgm-theraspective-test.py
+++ tgm-theraspective-test.py
@@ -1,48 +1,73 @@
-from flask import Flask, render_template
-import requests
-import os
+from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import os
 from time import sleep
 
 app = Flask(__name__)
 
 BASE_URL = "https://www.iheartjane.com/brands/34404/camino/products/1184332/camino-10-3-sleep-blackberry-dream-10-pk-100-mg-thc-30-mg-cbn?page={}"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
-
 @app.route('/')
 def scrape_reviews():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
     page = 1
     all_reviews = []
-    session = requests.Session()  # Use session for consistent headers and cookies.
+    
+    try:
+        while True:
+            driver.get(BASE_URL.format(page))
 
-    while True:
-        try:
-            response = session.get(BASE_URL.format(page), headers=headers, timeout=15)
-            response.raise_for_status()
-            
-            soup = BeautifulSoup(response.content, 'html.parser')
+            # Handling the age verification popup
+            try:
+                confirm_button = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "button[name='Confirm'].css-a9cjuw"))
+                )
+                confirm_button.click()
+            except:
+                pass  # If not found, just continue
+
+            # Handling the address input popup
+            try:
+                close_button = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "div.css-swyrt1 div[role='button'].css-kw7h5n"))
+                )
+                close_button.click()
+            except:
+                pass  # If not found, just continue
+
+            sleep(2)  # Pause for 2 seconds for the page to load properly
+
+            # Parsing the page content with BeautifulSoup
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
             reviews_list = soup.find('ul', {'class': 'ais-Hits-list'})
             
             if reviews_list:
                 reviews = [li.text.strip() for li in reviews_list.find_all('li', {'class': 'ais-Hits-item'})]
                 all_reviews.extend(reviews)
+
+                prev_page_button = soup.find('button', {'aria-label': 'previous-page', 'class': 'css-d85ps1'})
+                if prev_page_button is None:
+                    break
+
+                page += 1
+                sleep(2)  # Pause for 2 seconds before the next request
             else:
-                print("Error: reviews_list not found!")
-                break  # Stop if reviews_list not found or adjust based on your needs.
-            
-            prev_page_button = soup.find('button', {'aria-label': 'previous-page', 'class': 'css-d85ps1'})
-            if prev_page_button is None:
                 break
-
-            page += 1
-            sleep(2)  # Pause for 2 seconds before the next request
-
-        except requests.RequestException as e:
-            print(f"Error fetching page {page}: {e}")
-            break  # Stop scraping on error.
+    except Exception as e:
+        return f"Error: {str(e)}"
+    finally:
+        driver.quit()
 
     return '<br>'.join(all_reviews)
 
